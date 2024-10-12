0e207108fdedd081b5379aaf804ed0fff55d4eff
Amit Biswas <amit.142biswas@gmail.com>
2024-10-12 15:47:53 +0530

    sfvs

---
+++ src/store/Auth.ts
@@ -0,0 +1,11 @@
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
+import { persist } from "zustand/middleware";
+
+import { AppwriteException, ID, Models } from "appwrite";
+import { account } from "@/models/client/config";
+
+export interface UserPrefs {
+    reputation: number
+}
+
