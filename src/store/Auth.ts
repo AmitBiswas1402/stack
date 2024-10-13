3d5f6b01e3a86488271e1a8fc482bb62b445548c
Amit Biswas <amit.142biswas@gmail.com>
2024-10-13 21:20:46 +0530

    fhn

--- src/store/Auth.ts
+++ src/store/Auth.ts
@@ -6,6 +6,32 @@ import { AppwriteException, ID, Models } from "appwrite";
 import { account } from "@/models/client/config";
 
 export interface UserPrefs {
-    reputation: number
+  reputation: number;
+}
+
+interface IAuthStore {
+  session: Models.Session | null;
+  jwt: string | null;
+  user: Models.User<UserPrefs> | null;
+  hydrated: boolean;
+
+  setHydrated(): void;
+  verfiySession(): Promise<void>;
+  login(
+    email: string,
+    password: string
+  ): Promise<{
+    success: boolean;
+    error?: AppwriteException | null;
+  }>;
+  createAccount(
+    name: string,
+    email: string,
+    password: string
+  ): Promise<{
+    success: boolean;
+    error?: AppwriteException | null;
+  }>;
+  logout(): Promise<void>;
 }
 
